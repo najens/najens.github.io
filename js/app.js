6ba2710afd5d09342b2c6f8e1181736c40400128
najens <kanimambo15@gmail.com>
2017-12-06 18:55:16 -0700

    v1.0

---
+++ js/app.js
@@ -0,0 +1,311 @@
+// Get items from html
+var title = document.getElementById("title");
+var langBtn = document.getElementById("lang-btn");
+var langBtnTxt = document.getElementById("lang-btn-txt");
+var lang = document.getElementById("lang");
+var langItems = lang.getElementsByTagName("a");
+var en = document.getElementById("en");
+var es = document.getElementById("es");
+var pt = document.getElementById("pt");
+var navButton = document.getElementById("nav-btn");
+var navList = document.getElementById("nav-lst");
+var navListItems = navList.getElementsByTagName("a");
+var home = document.getElementById("home");
+var section1 = document.getElementById("section-1");
+var section2 = document.getElementById("section-2");
+var section3 = document.getElementById("section-3");
+var introGreeting = document.getElementById("intro-greeting");
+var introName = document.getElementById("intro-name");
+var introText = document.getElementById("intro-text");
+var introLink = document.getElementById("intro-link");
+var aboutHeading = document.getElementById("about-heading");
+var aboutImg1 = document.getElementById("about-img-1");
+var aboutSub1 = document.getElementById("about-sub-1");
+var aboutText1 = document.getElementById("about-text-1");
+var aboutSub2 = document.getElementById("about-sub-2");
+var aboutText2 = document.getElementById("about-text-2");
+var aboutSub3 = document.getElementById("about-sub-3");
+var aboutText3 = document.getElementById("about-text-3");
+var skillsHeading = document.getElementById("skills-heading");
+var skill1 = document.getElementById("skill-1");
+var percent1 = document.getElementById("percent-1");
+var skill2 = document.getElementById("skill-2");
+var percent2 = document.getElementById("percent-2");
+var skill3 = document.getElementById("skill-3");
+var percent3 = document.getElementById("percent-3");
+var skill4 = document.getElementById("skill-4");
+var percent4 = document.getElementById("percent-4");
+var skill5 = document.getElementById("skill-5");
+var percent5 = document.getElementById("percent-5");
+var skill6 = document.getElementById("skill-6");
+var percent6 = document.getElementById("percent-6");
+var skill7 = document.getElementById("skill-7");
+var percent7 = document.getElementById("percent-7");
+var skill8 = document.getElementById("skill-8");
+var percent8 = document.getElementById("percent-8");
+var skill9 = document.getElementById("skill-9");
+var percent9 = document.getElementById("percent-9");
+var skill10 = document.getElementById("skill-10");
+var percent10 = document.getElementById("percent-10");
+var skillLabel1 = document.getElementById("skill-label-1");
+var skillLabel2 = document.getElementById("skill-label-2");
+var skillLabel3 = document.getElementById("skill-label-3");
+var skillLabel4 = document.getElementById("skill-label-4");
+var portfolioHeading = document.getElementById("portfolio-heading");
+var portfolioProject1 = document.getElementById("portfolio-project-1");
+var portfolioTopic1 = document.getElementById("portfolio-topic-1");
+var portfolioProject2 = document.getElementById("portfolio-project-2");
+var portfolioTopic2 = document.getElementById("portfolio-topic-2");
+var copyright = document.getElementById("copyright");
+var newDiv = document.createElement("div");
+var newContent = document.createTextNode("Hi there and greetings!");
+
+// Display text at start up
+title.innerHTML = titleEN.title;
+langBtnTxt.innerHTML = languagesEN.english;
+en.innerHTML = languagesEN.english;
+es.innerHTML = languagesEN.spanish;
+pt.innerHTML = languagesEN.portuguese;
+home.innerHTML = navEN.home;
+section1.innerHTML = navEN.section1;
+section2.innerHTML = navEN.section2;
+section3.innerHTML = navEN.section3;
+introGreeting.innerHTML = homeEN.text1;
+introName.innerHTML = homeEN.text2;
+introText.innerHTML = homeEN.text3;
+introLink.innerHTML = homeEN.text4;
+aboutHeading.innerHTML = aboutEN.heading;
+// TODO: set alt text on image
+aboutSub1.innerHTML = aboutEN.subheading1;
+aboutText1.innerHTML = aboutEN.text1;
+aboutSub2.innerHTML = aboutEN.subheading2;
+aboutText2.innerHTML = aboutEN.text2;
+aboutSub3.innerHTML = aboutEN.subheading3;
+aboutText3.innerHTML = aboutEN.text3;
+skillsHeading.innerHTML = skillsEN.heading;
+skill1.innerHTML = skillsEN.skill1;
+percent1.innerHTML = skillsEN.percent1;
+skill2.innerHTML = skillsEN.skill2;
+percent2.innerHTML = skillsEN.percent2;
+skill3.innerHTML = skillsEN.skill3;
+percent3.innerHTML = skillsEN.percent3;
+skill4.innerHTML = skillsEN.skill4;
+percent4.innerHTML = skillsEN.percent4;
+skill5.innerHTML = skillsEN.skill5;
+percent5.innerHTML = skillsEN.percent5;
+skill6.innerHTML = skillsEN.skill6;
+percent6.innerHTML = skillsEN.percent6;
+skill7.innerHTML = skillsEN.skill7;
+percent7.innerHTML = skillsEN.percent7;
+skill8.innerHTML = skillsEN.skill8;
+percent8.innerHTML = skillsEN.percent8;
+skill9.innerHTML = skillsEN.skill9;
+percent9.innerHTML = skillsEN.percent9;
+skill10.innerHTML = skillsEN.skill10;
+percent10.innerHTML = skillsEN.percent10;
+skillLabel1.innerHTML = skillsEN.label1;
+skillLabel2.innerHTML = skillsEN.label2;
+skillLabel3.innerHTML = skillsEN.label3;
+skillLabel4.innerHTML = skillsEN.label4;
+portfolioHeading.innerHTML = portfolioEN.heading;
+portfolioProject1.innerHTML = portfolioEN.project1;
+portfolioTopic1.innerHTML = portfolioEN.topic1;
+portfolioProject2.innerHTML = portfolioEN.project2;
+portfolioTopic2.innerHTML = portfolioEN.topic2;
+copyright.innerHTML = footerEN.copyright;
+
+
+// Display text in English on click
+en.addEventListener("click", function() {
+  // Display text at start up
+  title.innerHTML = titleEN.title;
+  langBtnTxt.innerHTML = languagesEN.english;
+  en.innerHTML = languagesEN.english;
+  es.innerHTML = languagesEN.spanish;
+  pt.innerHTML = languagesEN.portuguese;
+  home.innerHTML = navEN.home;
+  section1.innerHTML = navEN.section1;
+  section2.innerHTML = navEN.section2;
+  section3.innerHTML = navEN.section3;
+  introGreeting.innerHTML = homeEN.text1;
+  introName.innerHTML = homeEN.text2;
+  introText.innerHTML = homeEN.text3;
+  introLink.innerHTML = homeEN.text4;
+  aboutHeading.innerHTML = aboutEN.heading;
+  // TODO: set alt text on image
+  aboutSub1.innerHTML = aboutEN.subheading1;
+  aboutText1.innerHTML = aboutEN.text1;
+  aboutSub2.innerHTML = aboutEN.subheading2;
+  aboutText2.innerHTML = aboutEN.text2;
+  aboutSub3.innerHTML = aboutEN.subheading3;
+  aboutText3.innerHTML = aboutEN.text3;
+  skillsHeading.innerHTML = skillsEN.heading;
+  skill1.innerHTML = skillsEN.skill1;
+  percent1.innerHTML = skillsEN.percent1;
+  skill2.innerHTML = skillsEN.skill2;
+  percent2.innerHTML = skillsEN.percent2;
+  skill3.innerHTML = skillsEN.skill3;
+  percent3.innerHTML = skillsEN.percent3;
+  skill4.innerHTML = skillsEN.skill4;
+  percent4.innerHTML = skillsEN.percent4;
+  skill5.innerHTML = skillsEN.skill5;
+  percent5.innerHTML = skillsEN.percent5;
+  skill6.innerHTML = skillsEN.skill6;
+  percent6.innerHTML = skillsEN.percent6;
+  skill7.innerHTML = skillsEN.skill7;
+  percent7.innerHTML = skillsEN.percent7;
+  skill8.innerHTML = skillsEN.skill8;
+  percent8.innerHTML = skillsEN.percent8;
+  skill9.innerHTML = skillsEN.skill9;
+  percent9.innerHTML = skillsEN.percent9;
+  skill10.innerHTML = skillsEN.skill10;
+  percent10.innerHTML = skillsEN.percent10;
+  skillLabel1.innerHTML = skillsEN.label1;
+  skillLabel2.innerHTML = skillsEN.label2;
+  skillLabel3.innerHTML = skillsEN.label3;
+  skillLabel4.innerHTML = skillsEN.label4;
+  portfolioHeading.innerHTML = portfolioEN.heading;
+  portfolioProject1.innerHTML = portfolioEN.project1;
+  portfolioTopic1.innerHTML = portfolioEN.topic1;
+  portfolioProject2.innerHTML = portfolioEN.project2;
+  portfolioTopic2.innerHTML = portfolioEN.topic2;
+  copyright.innerHTML = footerEN.copyright;
+});
+
+// Display text in Spanish on click
+es.addEventListener("click", function() {
+  title.innerHTML = titleES.title;
+  langBtnTxt.innerHTML = languagesES.spanish;
+  en.innerHTML = languagesES.english;
+  es.innerHTML = languagesES.spanish;
+  pt.innerHTML = languagesES.portuguese;
+  home.innerHTML = navES.home;
+  section1.innerHTML = navES.section1;
+  section2.innerHTML = navES.section2;
+  section3.innerHTML = navES.section3;
+  introGreeting.innerHTML = homeES.text1;
+  introName.innerHTML = homeES.text2;
+  introText.innerHTML = homeES.text3;
+  introLink.innerHTML = homeES.text4;
+  aboutHeading.innerHTML = aboutES.heading;
+  // TODO: set alt text on image
+  aboutSub1.innerHTML = aboutES.subheading1;
+  aboutText1.innerHTML = aboutES.text1;
+  aboutSub2.innerHTML = aboutES.subheading2;
+  aboutText2.innerHTML = aboutES.text2;
+  aboutSub3.innerHTML = aboutES.subheading3;
+  aboutText3.innerHTML = aboutES.text3;
+  skillsHeading.innerHTML = skillsES.heading;
+  skill1.innerHTML = skillsES.skill1;
+  percent1.innerHTML = skillsES.percent1;
+  skill2.innerHTML = skillsES.skill2;
+  percent2.innerHTML = skillsES.percent2;
+  skill3.innerHTML = skillsES.skill3;
+  percent3.innerHTML = skillsES.percent3;
+  skill4.innerHTML = skillsES.skill4;
+  percent4.innerHTML = skillsES.percent4;
+  skill5.innerHTML = skillsES.skill5;
+  percent5.innerHTML = skillsES.percent5;
+  skill6.innerHTML = skillsES.skill6;
+  percent6.innerHTML = skillsES.percent6;
+  skill7.innerHTML = skillsES.skill7;
+  percent7.innerHTML = skillsES.percent7;
+  skill8.innerHTML = skillsES.skill8;
+  percent8.innerHTML = skillsES.percent8;
+  skill9.innerHTML = skillsES.skill9;
+  percent9.innerHTML = skillsES.percent9;
+  skill10.innerHTML = skillsES.skill10;
+  percent10.innerHTML = skillsES.percent10;
+  skillLabel1.innerHTML = skillsES.label1;
+  skillLabel2.innerHTML = skillsES.label2;
+  skillLabel3.innerHTML = skillsES.label3;
+  skillLabel4.innerHTML = skillsES.label4;
+  portfolioHeading.innerHTML = portfolioES.heading;
+  portfolioProject1.innerHTML = portfolioES.project1;
+  portfolioTopic1.innerHTML = portfolioES.topic1;
+  portfolioProject2.innerHTML = portfolioES.project2;
+  portfolioTopic2.innerHTML = portfolioES.topic2;
+  copyright.innerHTML = footerES.copyright;
+});
+
+
+
+// Display text in Portuguese on click
+pt.addEventListener("click", function() {
+  title.innerHTML = titlePT.title;
+  langBtnTxt.innerHTML = languagesPT.portuguese;
+  en.innerHTML = languagesPT.english;
+  es.innerHTML = languagesPT.spanish;
+  pt.innerHTML = languagesPT.portuguese;
+  home.innerHTML = navPT.home;
+  section1.innerHTML = navPT.section1;
+  section2.innerHTML = navPT.section2;
+  section3.innerHTML = navPT.section3;
+  introGreeting.innerHTML = homePT.text1;
+  introName.innerHTML = homePT.text2;
+  introText.innerHTML = homePT.text3;
+  introLink.innerHTML = homePT.text4;
+  aboutHeading.innerHTML = aboutPT.heading;
+  // TODO: set alt text on image
+  aboutSub1.innerHTML = aboutPT.subheading1;
+  aboutText1.innerHTML = aboutPT.text1;
+  aboutSub2.innerHTML = aboutPT.subheading2;
+  aboutText2.innerHTML = aboutPT.text2;
+  aboutSub3.innerHTML = aboutPT.subheading3;
+  aboutText3.innerHTML = aboutPT.text3;
+  skillsHeading.innerHTML = skillsPT.heading;
+  skill1.innerHTML = skillsPT.skill1;
+  percent1.innerHTML = skillsPT.percent1;
+  skill2.innerHTML = skillsPT.skill2;
+  percent2.innerHTML = skillsPT.percent2;
+  skill3.innerHTML = skillsPT.skill3;
+  percent3.innerHTML = skillsPT.percent3;
+  skill4.innerHTML = skillsPT.skill4;
+  percent4.innerHTML = skillsPT.percent4;
+  skill5.innerHTML = skillsPT.skill5;
+  percent5.innerHTML = skillsPT.percent5;
+  skill6.innerHTML = skillsPT.skill6;
+  percent6.innerHTML = skillsPT.percent6;
+  skill7.innerHTML = skillsPT.skill7;
+  percent7.innerHTML = skillsPT.percent7;
+  skill8.innerHTML = skillsPT.skill8;
+  percent8.innerHTML = skillsPT.percent8;
+  skill9.innerHTML = skillsPT.skill9;
+  percent9.innerHTML = skillsPT.percent9;
+  skill10.innerHTML = skillsPT.skill10;
+  percent10.innerHTML = skillsPT.percent10;
+  skillLabel1.innerHTML = skillsPT.label1;
+  skillLabel2.innerHTML = skillsPT.label2;
+  skillLabel3.innerHTML = skillsPT.label3;
+  skillLabel4.innerHTML = skillsPT.label4;
+  portfolioHeading.innerHTML = portfolioPT.heading;
+  portfolioProject1.innerHTML = portfolioPT.project1;
+  portfolioTopic1.innerHTML = portfolioPT.topic1;
+  portfolioProject2.innerHTML = portfolioPT.project2;
+  portfolioTopic2.innerHTML = portfolioPT.topic2;
+  copyright.innerHTML = footerPT.copyright;
+});
+
+// Display nav list on click
+navButton.addEventListener("click", function() {
+  navList.style.display = navList.style.display == "flex" ? "none" : "flex";
+});
+
+// Hide nav list when nav list item is clicked
+for (var i=0; i<navListItems.length; i++) {
+  navListItems[i].addEventListener("click", function() {
+    navList.style.display = navList.style.display == "flex" ? "none" : "flex";
+  });
+}
+
+// Display language list on click
+langBtn.addEventListener("click", function() {
+  lang.style.display = lang.style.display == "block" ? "none" : "block";
+})
+
+// Hide language list when lang list item is clicked
+for (var i=0; i<langItems.length; i++) {
+  langItems[i].addEventListener("click", function() {
+    lang.style.display = lang.style.display == "block" ? "none" : "block";
+  })
+}
---
+++ js/data.js
@@ -0,0 +1,233 @@
+// English
+var titleEN = {
+  title: "Nathan Jensen | Developer Portfolio"
+}
+
+var languagesEN = {
+  english: "English",
+  spanish: "Español",
+  portuguese: "Português"
+}
+
+var navEN = {
+  home: "Home",
+  section1: "About",
+  section2: "Skills",
+  section3: "Portfolio"
+}
+
+var homeEN = {
+  text1: "Hello, I\'m ",
+  text2: "Nathan Jensen",
+  text3: "I\'m a full-stack web developer!",
+  text4: "View my work"
+}
+
+var aboutEN = {
+  heading: "About",
+  alt1: "Me",
+  subheading1: "Me",
+  text1: "I\'m a freelance web and Android developer in Provo, UT. I am " +
+    "passionate about developing data driven apps with elegant and easy to " +
+    "use interfaces. Let\'s make something special.",
+  subheading2: "Responsive",
+  text2: "My layouts will work on any device, big or small",
+  subheading3: "Fast",
+  text3: "Fast load times and lag free interaction, my highest priority"
+}
+
+var skillsEN = {
+  heading: "Skills",
+  skill1: "CSS",
+  percent1: "40%",
+  skill2: "HTML",
+  percent2: "50%",
+  skill3: "JavaScript",
+  percent3: "20%",
+  skill4: "Python",
+  percent4: "30%",
+  skill5: "Java",
+  percent5: "60%",
+  skill6: "Android",
+  percent6: "50%",
+  skill7: "UI Design",
+  percent7: "40%",
+  skill8: "Git",
+  percent8: "60%",
+  skill9: "PostgreSQL",
+  percent9: "60%",
+  skill10: "AWS",
+  percent10: "25%",
+  label1: "Newbie",
+  label2: "Geek",
+  label3: "Ninja",
+  label4: "Jedi"
+}
+
+var portfolioEN = {
+  heading: "Portfolio",
+  project1: "Movie Trailer Website",
+  topic1: "Python/Web",
+  project2: "Mobile News App",
+  topic2: "Android/APIs"
+}
+
+var footerEN = {
+  copyright: "© Nathan Jensen"
+}
+
+// Spanish
+var titleES = {
+  title: "Nathan Jensen | Portafolio Desarrollo"
+}
+
+var languagesES = {
+  english: "English",
+  spanish: "Español",
+  portuguese: "Português"
+}
+
+var navES = {
+  home: "Página Principal",
+  section1: "Sobre",
+  section2: "Habilidades",
+  section3: "Portafolio"
+}
+
+var homeES = {
+  text1: "Hola, soy ",
+  text2: "Nathan Jensen",
+  text3: "¡Soy un desarrollador web completo!",
+  text4: "Mira mi trabajo"
+}
+
+var aboutES = {
+  heading: "Sobre",
+  alt1: "Yo",
+  subheading1: "Yo",
+  text1: "Soy desarrollador web y Android independiente en Provo, UT. Me " +
+  "apasiona el desarrollo de aplicaciones basadas en datos con elegante y " +
+  "fácil de usar interfaces. Vamos a crear algo especial.",
+  subheading2: "Adaptativo",
+  text2: "Mis diseños funcionan muy bien en todos los tamaños de pantalla",
+  subheading3: "Rápido",
+  text3: "Los tiempos de carga rápidos son mi más alta prioridad"
+}
+
+var skillsES = {
+  heading: "Habilidades",
+  skill1: "CSS",
+  percent1: "40%",
+  skill2: "HTML",
+  percent2: "50%",
+  skill3: "JavaScript",
+  percent3: "20%",
+  skill4: "Python",
+  percent4: "30%",
+  skill5: "Java",
+  percent5: "60%",
+  skill6: "Android",
+  percent6: "50%",
+  skill7: "UI Design",
+  percent7: "40%",
+  skill8: "Git",
+  percent8: "60%",
+  skill9: "PostgreSQL",
+  percent9: "60%",
+  skill10: "AWS",
+  percent10: "25%",
+  label1: "Principiante",
+  label2: "Intermedio",
+  label3: "Avanzado",
+  label4: "Experto"
+}
+
+var portfolioES = {
+  heading: "Portafolio",
+  project1: "Sitio de Trailers de Películas",
+  topic1: "Python/Web",
+  project2: "Aplicación de Noticias",
+  topic2: "Android/APIs"
+}
+
+var footerES = {
+  copyright: "© Nathan Jensen"
+}
+
+// Portuguese
+var titlePT = {
+  title: "Nathan Jensen | Portfólio Desenvolvimento"
+}
+
+var languagesPT = {
+  english: "English",
+  spanish: "Español",
+  portuguese: "Português"
+}
+
+var navPT = {
+  home: "Pagina Inicial",
+  section1: "Sobre",
+  section2: "Habilidades",
+  section3: "Portfólio"
+}
+
+var homePT = {
+  text1: "Olá, sou ",
+  text2: "Nathan Jensen",
+  text3: "Sou um desenvolvedor web completo!",
+  text4: "Confira meu trabalho"
+}
+
+var aboutPT = {
+  heading: "Sobre",
+  alt1: "Eu",
+  subheading1: "Eu",
+  text1: "Sou desenvolvedor web y Android independente em Provo, UT. Estou " +
+  "apaixonado por desenvolver aplicativos orientados a dados com interfaces " +
+  "fáceis de usar. Vamos construir algo especial.",
+  subheading2: "Responsivo",
+  text2: "Meus projetos funcionam bem em todos os tamanhos de tela",
+  subheading3: "Rápido",
+  text3: "Os tempos de carregamento rápido são a minha maior prioridade"
+}
+
+var skillsPT = {
+  heading: "Habilidades",
+  skill1: "CSS",
+  percent1: "40%",
+  skill2: "HTML",
+  percent2: "50%",
+  skill3: "JavaScript",
+  percent3: "20%",
+  skill4: "Python",
+  percent4: "30%",
+  skill5: "Java",
+  percent5: "60%",
+  skill6: "Android",
+  percent6: "50%",
+  skill7: "UI Design",
+  percent7: "40%",
+  skill8: "Git",
+  percent8: "60%",
+  skill9: "PostgreSQL",
+  percent9: "60%",
+  skill10: "AWS",
+  percent10: "25%",
+  label1: "Principiante",
+  label2: "Intermediário",
+  label3: "Avançado",
+  label4: "Especialista"
+}
+
+var portfolioPT = {
+  heading: "Portfólio",
+  project1: "Site de Trailers de Filmes",
+  topic1: "Python/Web",
+  project2: "Aplicativo de Notícias",
+  topic2: "Android/APIs"
+}
+
+var footerPT = {
+  copyright: "© Nathan Jensen"
+}
